12744c4863add5647adb743036142e3b71649679
Liam Boyle <liam_boyle@hotmail.com>
2019-12-27 18:34:34 +0000

    Stopped purging being done in background to make sure it finishes

--- set-azure-storage-content-types.ps1
+++ set-azure-storage-content-types.ps1
@@ -46,7 +46,7 @@ foreach ($Blob in $Blobs)
 $ResourceGroup = 'fileconversion'
 $EndpointName = 'fileconversiontools'
 $ProfileName = 'FileConversioNCDN'
-Unpublish-AzCdnEndpointContent -ProfileName $ProfileName -ResourceGroupName $ResourceGroup -EndpointName $EndpointName -PurgeContent "/*" &
+Unpublish-AzCdnEndpointContent -ProfileName $ProfileName -ResourceGroupName $ResourceGroup -EndpointName $EndpointName -PurgeContent "/*"
 
 #remove dist directory now we are finished with it so its clean for next run
 Remove-Item -Recurse -Force FileConvert\bin\Release\netstandard2.1\publish\FileConvert\dist -ErrorAction SilentlyContinue
