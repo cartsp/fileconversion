4040d6ec3be5f0347d06988b41917865f0b795ff
Liam Boyle <liam_boyle@hotmail.com>
2019-12-27 18:25:10 +0000

    Changes to master publish to purge cdn

--- set-azure-storage-content-types.ps1
+++ set-azure-storage-content-types.ps1
@@ -41,5 +41,12 @@ foreach ($Blob in $Blobs)
 
     $CloudBlockBlob.SetProperties()    
 }
+
+#purge cdn after deployment
+$ResourceGroup = 'fileconversion'
+$EndpointName = 'fileconversiontools'
+$ProfileName = 'FileConversioNCDN'
+Unpublish-AzCdnEndpointContent -ProfileName $ProfileName -ResourceGroupName $ResourceGroup -EndpointName $EndpointName -PurgeContent "/*" &
+
 #remove dist directory now we are finished with it so its clean for next run
 Remove-Item -Recurse -Force FileConvert\bin\Release\netstandard2.1\publish\FileConvert\dist -ErrorAction SilentlyContinue
